7e78486df59d847905218d370bdceaa53b0727a2
vnramachandra <ramachandra.naragund@robosoftin.com>
2015-11-10 19:44:16 +0530

    Modified and added cocoapods with swift sample

---
+++ SwiftExample/SwiftExample/Pyze-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Pyze-Bridging-Header.h
+//  SwiftExample
+//
+//  Created by Ramachandra Naragund on 09/11/15.
+//  Copyright © 2015 Pyze Technologies Pvt Ltd. All rights reserved.
+//
+
+#ifndef Pyze_Bridging_Header_h
+#define Pyze_Bridging_Header_h
+
+#import <Pyze/Pyze.h>
+
+#endif /* Pyze_Bridging_Header_h */
