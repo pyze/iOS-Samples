11d046d79e2bc364ff5395b3aea43c33540df5ce
vnramachandra <ramachandra.naragund@robosoftin.com>
2016-08-19 21:22:40 +0530

    Added InAppTest with v2.5.3

---
+++ InAppTest/InAppTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InAppTest
+//
+//  Created by Ramachandra Naragund on 19/08/16.
+//  Copyright © 2016 Ramachandra Naragund. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InAppTest/InAppTest/ViewController.m
@@ -0,0 +1,64 @@
+//
+//  ViewController.m
+//  InAppTest
+//
+//  Created by Ramachandra Naragund on 19/08/16.
+//  Copyright © 2016 Ramachandra Naragund. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <Pyze/Pyze.h>
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *showMessagesButton;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [Pyze addBadge:self.showMessagesButton]; //1
+}
+
+- (IBAction)didTapShowMessage:(id)sender {
+    [Pyze showInAppNotificationUIForDisplayMessages:PyzeInAppTypeAll
+                          msgNavBarButtonsTextColor:[UIColor blackColor]
+                            msgNavBarButtonsBgColor:[UIColor lightGrayColor]
+                                   msgNavBarBgColor:[UIColor lightGrayColor]
+                          msgNavBarCounterTextColor:[UIColor whiteColor]
+                              withCompletionHandler:^(PyzeInAppStatus *inAppStatus) {
+                                  NSLog(@"buttonIndex = %d", (int)inAppStatus.buttonIndex);
+                                  NSLog(@"message-ID =%@" , inAppStatus.messageID);
+                                  NSLog(@"campaign-ID = %@", inAppStatus.campaignID);
+                                  NSLog(@"title = %@",inAppStatus.title);
+                                  NSLog(@"urlString = %@",inAppStatus.urlString);
+                              }];
+}
+
+
+@end
+
+/* Swift -version
+
+class ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Pyze.addBadge(self.showMessagesButton)
+        //1
+    }
+    
+    @IBAction func didTapShowMessage(sender: AnyObject) {
+        Pyze.showInAppNotificationUIForDisplayMessages(PyzeInAppTypeAll, msgNavBarButtonsTextColor: UIColor.blackColor(), msgNavBarButtonsBgColor: UIColor.lightGrayColor(), msgNavBarBgColor: UIColor.lightGrayColor(), msgNavBarCounterTextColor: UIColor.whiteColor(), withCompletionHandler: {(inAppStatus: PyzeInAppStatus) -> Void in
+            print("buttonIndex = \(Int(inAppStatus.buttonIndex))")
+            print("message-ID =\(inAppStatus.messageID)")
+            print("campaign-ID = \(inAppStatus.campaignID)")
+            print("title = \(inAppStatus.title)")
+            print("urlString = \(inAppStatus.urlString)")
+        })
+    }
+}
+*/
+
